d003aeb5e3b9ea6489e397bb74daf65da862b7d2
raykrajci <Raymond.Krajci@gmail.com>
2025-09-10 16:43:25 -0400

    Release 0.1.2 with fix for case when all middle name fields are left blank.

--- R/generate.R
+++ R/generate.R
@@ -15,13 +15,17 @@ generate <- function(file = "dramatis.personae.yaml"){
     dplyr::rename_with(\(col) paste("affl", col, sep = "_"), dplyr::everything())
 
   authors <- dplyr::bind_rows(temp$authors) |>
+    purrr::when(
+      !"middle" %in% names(.) ~ dplyr::mutate(. , middle = NA_character_), #Add "middle" if it doesn't exist
+      TRUE ~ . #Otherwise, return as-is
+    ) |>
     tidyr::nest(affl = affl) |>
     dplyr::mutate(number = dplyr::row_number()) |>
     tidyr::unnest(affl) |>
     dplyr::rename_with(\(col) paste("auth", col, sep = "_"), dplyr::everything())
 
   temp <- dplyr::full_join(authors, affiliations, by = dplyr::join_by(auth_affl == affl_id)) |>
-    tidyr::nest(authors = c(auth_number, auth_first, auth_middle, auth_last)) |>
+    tidyr::nest(authors = dplyr::starts_with("auth")) |>
     dplyr::mutate(affl_number = dplyr::row_number()) |>
     tidyr::unnest(authors) |>
     dplyr::arrange(auth_number, affl_number)
